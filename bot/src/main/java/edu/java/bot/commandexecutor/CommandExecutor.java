d9f012a84db124040672a4d265d681a99676bf3f
Elijah <ilyasmolnikov567@gmail.com>
2024-02-19 00:03:44 +0300

    Disable link preview

--- bot/src/main/java/edu/java/bot/commandexecutor/CommandExecutor.java
+++ bot/src/main/java/edu/java/bot/commandexecutor/CommandExecutor.java
@@ -3,6 +3,7 @@ package edu.java.bot.commandexecutor;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.services.ResponseService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,13 @@ public class CommandExecutor implements UpdatesListener {
 
     @Override
     public int process(List<Update> list) {
-        list.forEach(update -> bot.execute(responseService.prepareResponse(update)));
+        list.forEach(update -> {
+            SendMessage message =
+                responseService
+                    .prepareResponse(update)
+                    .disableWebPagePreview(true);
+            bot.execute(message);
+        });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
