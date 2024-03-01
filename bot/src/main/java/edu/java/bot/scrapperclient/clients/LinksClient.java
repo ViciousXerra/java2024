65fd5094c6608e69aae7e60093ca847be24c65d9
Elijah <ilyasmolnikov567@gmail.com>
2024-03-01 03:33:39 +0300

    ???

--- bot/src/main/java/edu/java/bot/scrapperclient/clients/LinksClient.java
+++ bot/src/main/java/edu/java/bot/scrapperclient/clients/LinksClient.java
@@ -1,9 +1,9 @@
 package edu.java.bot.scrapperclient.clients;
 
 import edu.java.bot.scrapperclient.dto.requests.AddLinkRequest;
+import edu.java.bot.scrapperclient.dto.requests.RemoveLinkRequest;
 import edu.java.bot.scrapperclient.dto.responses.LinkResponse;
 import edu.java.bot.scrapperclient.dto.responses.ListLinkResponse;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,18 +15,18 @@ import org.springframework.web.service.annotation.PostExchange;
 public interface LinksClient {
 
     @GetExchange
-    ResponseEntity<ListLinkResponse> getAllLinks(@RequestHeader("Tg-Chat-Id") long chatId);
+    ListLinkResponse getAllLinks(@RequestHeader("Tg-Chat-Id") long chatId);
 
     @PostExchange
-    ResponseEntity<LinkResponse> addLink(
+    LinkResponse addLink(
         @RequestHeader("Tg-Chat-Id") long chatId,
         @RequestBody AddLinkRequest addLinkRequest
     );
 
     @DeleteMapping
-    ResponseEntity<LinkResponse> removeLink(
+    LinkResponse removeLink(
         @RequestHeader("Tg-Chat-Id") long chatId,
-        @RequestBody AddLinkRequest removeLinkRequest
+        @RequestBody RemoveLinkRequest removeLinkRequest
     );
 
 }
--- bot/src/test/java/edu/java/bot/scrapperclienttests/LinksClientTest.java
+++ bot/src/test/java/edu/java/bot/scrapperclienttests/LinksClientTest.java
@@ -1,7 +1,108 @@
 package edu.java.bot.scrapperclienttests;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import edu.java.bot.scrapperclient.clients.LinksClient;
+import edu.java.bot.scrapperclient.dto.requests.AddLinkRequest;
+import edu.java.bot.scrapperclient.dto.requests.RemoveLinkRequest;
+import edu.java.bot.scrapperclient.dto.responses.LinkResponse;
+import edu.java.bot.scrapperclient.dto.responses.ListLinkResponse;
+import java.net.URI;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@WireMockTest(httpPort = 8080)
+@SpringBootTest
 class LinksClientTest {
 
+    @Autowired
+    private LinksClient linksClient;
 
+    @DynamicPropertySource
+    static void stubScrapperBaseUrl(DynamicPropertyRegistry registry) {
+        registry.add("app.scrapper-settings.default-base-url", () -> "http://localhost:8080");
+    }
+
+    @Test
+    @DisplayName("Test GET exchange")
+    void testGetExchange() {
+        //set up
+        stubFor(get("/links").withHeader("Tg-Chat-Id", containing("1")).willReturn(okJson("""
+            {
+              "links": [
+                {
+                  "id": 1,
+                  "url": "https://github.com"
+                },
+                {
+                  "id": 2,
+                  "url": "https://stackoverflow.com"
+                }
+              ],
+              "size": 2
+            }""").withStatus(200)));
+        //Given
+        ListLinkResponse expectedResponse = new ListLinkResponse(
+            List.of(
+                new LinkResponse(1L, URI.create("https://github.com")),
+                new LinkResponse(2L, URI.create("https://stackoverflow.com"))
+            ),
+            2
+        );
+        //When
+        ListLinkResponse actualResponse = linksClient.getAllLinks(1L);
+        //Then
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    @DisplayName("Test POST exchange")
+    void testPostExchange() {
+        //set up
+        stubFor(post("/links").withHeader("Tg-Chat-Id", containing("1")).willReturn(okJson("""
+            {
+              "id": 1,
+              "url": "https://github.com"
+            }""").withStatus(200)));
+        //Given
+        LinkResponse expectedResponse = new LinkResponse(
+            1L,
+            URI.create("https://github.com")
+        );
+        //When
+        LinkResponse actualResponse = linksClient.addLink(1L, new AddLinkRequest("https://github.com"));
+        //Then
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    void testDeleteExchange() {
+        //set up
+        stubFor(delete("/links").withHeader("Tg-Chat-Id", containing("1")).willReturn(okJson("""
+            {
+              "id": 1,
+              "url": "https://github.com"
+            }""").withStatus(200)));
+        //Given
+        LinkResponse expectedResponse = new LinkResponse(
+            1L,
+            URI.create("https://github.com")
+        );
+        //When
+        LinkResponse actualResponse = linksClient.removeLink(1L, new RemoveLinkRequest("https://github.com"));
+        //Then
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
 
 }
