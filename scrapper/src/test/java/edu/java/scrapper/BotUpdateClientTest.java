1d556de9fabe2e49eaa553c223cd7eb0601333b6
Elijah <ilyasmolnikov567@gmail.com>
2024-03-01 18:40:43 +0300

    Create BotClient tests

---
+++ scrapper/src/test/java/edu/java/scrapper/BotUpdateClientTest.java
@@ -0,0 +1,94 @@
+package edu.java.scrapper;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import edu.java.scrapper.telegrambotclient.clients.BotUpdateClient;
+import edu.java.scrapper.telegrambotclient.dto.requests.LinkUpdate;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class BotUpdateClientTest {
+
+    private static WireMockServer mockServer;
+    @Autowired
+    private BotUpdateClient botClient;
+
+    @BeforeAll
+    public static void setUpMockServer() {
+        mockServer = new WireMockServer();
+        mockServer.start();
+        WireMock.configureFor("localhost", 8080);
+    }
+
+    @AfterEach
+    public void resetMockServerState() {
+        mockServer.resetAll();
+    }
+
+    @AfterAll
+    public static void stopMockServer() {
+        mockServer.stop();
+        mockServer.shutdown();
+    }
+
+    @DynamicPropertySource
+    static void stubScrapperBaseUrl(DynamicPropertyRegistry registry) {
+        registry.add("app.bot-settings.default-base-url", () -> "http://localhost:8080");
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Test POST exchange")
+    void testPostExchange() {
+        //Set up
+        mockServer
+            .stubFor(post(urlEqualTo("/updates"))
+                .willReturn(aResponse()
+                    .withStatus(200)));
+        //When
+        ResponseEntity<?> actualResponse = botClient.postLinkUpdate(
+            new LinkUpdate(1L, "https://github.com", "desc", List.of(1L, 2L, 3L))
+        );
+        //Then
+        assertThat(actualResponse.getStatusCode().is2xxSuccessful()).isTrue();
+    }
+
+    @Test
+    @Order(2)
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
+    @DisplayName("Test 4xx http status handler")
+    void testClientErrorHandler() {
+        //Set up
+        mockServer
+            .stubFor(post(urlEqualTo("/updates"))
+                .willReturn(aResponse()
+                    .withStatus(400)));
+        //Then
+        assertThatThrownBy(
+            () -> botClient.postLinkUpdate(new LinkUpdate(1L, "https://github.com", "desc", List.of(1L, 2L, 3L)))
+        )
+            .isInstanceOf(WebClientResponseException.class);
+    }
+
+}
