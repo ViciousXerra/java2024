e8edfd3de90aa987993e456fa4c011b2495218d1
Elijah <ilyasmolnikov567@gmail.com>
2024-03-11 03:11:34 +0300

    Connection pool problem fixed

--- scrapper/src/test/java/edu/java/scrapper/JdbcRepositoriesTest.java
+++ scrapper/src/test/java/edu/java/scrapper/JdbcRepositoriesTest.java
@@ -5,6 +5,7 @@ import edu.java.scrapper.dao.repository.jdbc.JdbcChatRepository;
 import edu.java.scrapper.dao.repository.jdbc.JdbcLinkRepository;
 import java.util.List;
 import javax.sql.DataSource;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.JdbcClient;
@@ -20,14 +21,11 @@ public class JdbcRepositoriesTest extends IntegrationTest {
     private JdbcLinkRepository linkRepository;
     @Autowired
     private JdbcChatIdLinkIdRepository chatIdLinkIdRepository;
+    @Autowired
     private JdbcClient jdbcClient;
 
-    @Autowired
-    public JdbcRepositoriesTest(DataSource dataSource) {
-        this.jdbcClient = JdbcClient.create(dataSource);
-    }
-
     @Test
+    @DisplayName("Test \"add\" chat repository method")
     @Transactional
     @Rollback
     void addChatIdTest() {
@@ -44,10 +42,33 @@ public class JdbcRepositoriesTest extends IntegrationTest {
     }
 
     @Test
+    @DisplayName("Test \"remove\" chat repository method")
     @Transactional
     @Rollback
     void removeChatIdTest() {
+        //Given
+        List<Long> expectedIds = List.of(1L, 3L);
+        //When
+        jdbcClient.sql("INSERT INTO Chat (id) VALUES (1), (2), (3)").update();
+        chatRepository.remove(2L);
+        List<Long> actualIds =
+            jdbcClient.sql("SELECT id FROM Chat").query((rs, rowCol) -> Long.valueOf(rs.getString("id"))).list();
+        //Then
+        assertThat(actualIds).containsOnlyOnceElementsOf(expectedIds);
+    }
 
+    @Test
+    @DisplayName("Test \"find all\" chat repository method")
+    @Transactional
+    @Rollback
+    void findAllChatIdTest() {
+        //Given
+        List<Long> expectedIds = List.of(1L, 2L, 3L, 4L, 5L);
+        //When
+        jdbcClient.sql("INSERT INTO Chat (id) VALUES (1), (2), (3), (4), (5)").update();
+        List<Long> actualIds = chatRepository.findAll();
+        //Then
+        assertThat(actualIds).containsOnlyOnceElementsOf(expectedIds);
     }
 
 }
