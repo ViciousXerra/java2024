f2bb66ac2d941568041c111ec2e8979397ea1474
Elijah <ilyasmolnikov567@gmail.com>
2024-03-14 00:03:19 +0300

    Refactoring: ChatClient.java LinksClient.java

--- bot/src/main/java/edu/java/bot/scrapperclient/clients/ChatClient.java
+++ bot/src/main/java/edu/java/bot/scrapperclient/clients/ChatClient.java
@@ -9,10 +9,12 @@ import org.springframework.web.service.annotation.PostExchange;
 @HttpExchange(url = "/tg-chat")
 public interface ChatClient {
 
-    @PostExchange(url = "/{id}")
+    String PATH_VAR_TEMPLATE = "/{id}";
+
+    @PostExchange(url = PATH_VAR_TEMPLATE)
     ResponseEntity<?> signUpChat(@PathVariable long id);
 
-    @DeleteExchange(url = "/{id}")
+    @DeleteExchange(url = PATH_VAR_TEMPLATE)
     ResponseEntity<?> removeChat(@PathVariable long id);
 
 }
