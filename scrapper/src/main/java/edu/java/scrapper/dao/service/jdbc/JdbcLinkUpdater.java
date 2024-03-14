55a80f6b13920a7c456c0204e58c0cfb150fe8c7
Elijah <ilyasmolnikov567@gmail.com>
2024-03-14 22:04:46 +0300

    Refactoring LinksRestController.java

---
+++ scrapper/src/main/java/edu/java/scrapper/dao/service/jdbc/JdbcLinkUpdater.java
@@ -0,0 +1,10 @@
+package edu.java.scrapper.dao.service.jdbc;
+
+import edu.java.scrapper.dao.service.interfaces.LinkUpdater;
+
+public class JdbcLinkUpdater implements LinkUpdater {
+    @Override
+    public int update() {
+        return 0;
+    }
+}
