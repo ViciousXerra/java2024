a53e8a037bc9242501c48ec985c89a3fafed69f6
Elijah <ilyasmolnikov567@gmail.com>
2024-03-17 02:57:31 +0300

    Redacted: JdbcLinkServiceTest.java JdbcLinkUpdaterTest.java

--- scrapper/src/test/java/edu/java/scrapper/scrapperrestcontrollerstests/LinksRestControllerTest.java
+++ scrapper/src/test/java/edu/java/scrapper/scrapperrestcontrollerstests/LinksRestControllerTest.java
@@ -1,176 +1,24 @@
 package edu.java.scrapper.scrapperrestcontrollerstests;
 
 import edu.java.scrapper.api.restcontrollers.LinksRestController;
-import edu.java.scrapper.dao.service.interfaces.ChatService;
 import edu.java.scrapper.dao.service.interfaces.LinkService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LinksRestController.class)
 class LinksRestControllerTest {
 
     @MockBean
-    LinkService linkService;
-
-    private static final String GET_RESPONSE_BODY =
-        """
-        {
-            "links": [
-                {
-                    "id": 1,
-                    "url": "https://github.com/ViciousXerra"
-                },
-                {
-                    "id": 1,
-                    "url": "https://stackoverflow.com"
-                }
-            ],
-            "size": 2
-        }
-        """;
-    private static final String INVALID_LINK_REQUEST_BODY =
-        """
-        {
-            "link": "not a valid url link"
-        }
-        """;
-    public static final String VALID_LINK_REQUEST_BODY =
-        """
-        {
-            "link": "https://github.com/ViciousXerra"
-        }
-        """;
-    public static final String VALID_RESPONSE_BODY1 = """
-        {
-            "id": 1,
-            "url": "https://stackoverflow.com"
-        }
-        """;
-    public static final String VALID_RESPONSE_BODY2 = """
-        {
-            "id": 1,
-            "url": "https://github.com/ViciousXerra"
-        }
-        """;
-
-    /*
-    TODO
-    Add MockBeans in future
-     */
-
+    private LinkService linkService;
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("Test GET \"get all links\" 200 OK")
-    void testGetAllLinksSuccess() throws Exception {
-        mockMvc.perform(
-                get("/scrapper/links").header("Tg-Chat-Id", 1L)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(content().json(GET_RESPONSE_BODY, false));
-    }
 
-    @Test
-    @DisplayName("Test GET \"get all links\" 400 Bad Request (Missing header)")
-    void testGetAllLinksBadRequestMissingHeader() throws Exception {
-        mockMvc.perform(
-                get("/scrapper/links")
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(content().contentType("application/json"));
-    }
-
-    @Test
-    @DisplayName("Test POST \"add link\" 200 OK")
-    void testPostAddLinkRequestSuccess() throws Exception {
-        mockMvc.perform(
-                post("/scrapper/links")
-                    .header("Tg-Chat-Id", 1L)
-                    .contentType("application/json")
-                    .content(VALID_LINK_REQUEST_BODY)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(content().json(VALID_RESPONSE_BODY1, false));
-    }
-
-    @Test
-    @DisplayName("Test POST \"add link\" 400 Bad Request (Missing header)")
-    void testPostAddLinkRequestBadRequestMissingHeader() throws Exception {
-        mockMvc.perform(
-                post("/scrapper/links")
-                    .contentType("application/json")
-                    .content(
-                        INVALID_LINK_REQUEST_BODY)
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(content().contentType("application/json"));
-    }
-
-    @Test
-    @DisplayName("Test POST \"add link\" 400 Bad Request")
-    void testPostAddLinkRequestBadRequest() throws Exception {
-        mockMvc.perform(
-                post("/scrapper/links")
-                    .header("Tg-Chat-Id", 1L)
-                    .contentType("application/json")
-                    .content(
-                        INVALID_LINK_REQUEST_BODY)
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(content().contentType("application/json"));
-    }
-
-    @Test
-    @DisplayName("Test DELETE \"remove link\" 200 OK")
-    void testDeleteRemoveLinkRequestSuccess() throws Exception {
-        mockMvc.perform(
-                delete("/scrapper/links")
-                    .header("Tg-Chat-Id", 1L)
-                    .contentType("application/json")
-                    .content(VALID_LINK_REQUEST_BODY)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(content().json(VALID_RESPONSE_BODY2, false));
-    }
-
-    @Test
-    @DisplayName("Test DELETE \"remove link\" 400 Bad Request (Missing header)")
-    void testDeleteRemoveLinkRequestBadRequestMissingHeader() throws Exception {
-        mockMvc.perform(
-                post("/scrapper/links")
-                    .contentType("application/json")
-                    .content(
-                        VALID_LINK_REQUEST_BODY)
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(content().contentType("application/json"));
-    }
-
-    @Test
-    @DisplayName("Test DELETE \"remove link\" 400 Bad Request")
-    void testDeleteRemoveLinkRequestBadRequest() throws Exception {
-        mockMvc.perform(
-                post("/scrapper/links")
-                    .header("Tg-Chat-Id", 1L)
-                    .contentType("application/json")
-                    .content(
-                        INVALID_LINK_REQUEST_BODY)
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(content().contentType("application/json"));
-    }
 
 }
--- scrapper/src/test/java/edu/java/scrapper/scrapperrestcontrollerstests/TelegramChatControllerTest.java
+++ scrapper/src/test/java/edu/java/scrapper/scrapperrestcontrollerstests/TelegramChatControllerTest.java
@@ -6,12 +6,15 @@ import edu.java.scrapper.api.restcontrollers.TelegramChatController;
 import edu.java.scrapper.dao.service.interfaces.ChatService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,6 +30,9 @@ class TelegramChatControllerTest {
     private static final String ERROR_RESPONSE_EXCEPTION_NAME_PATH = "$.exceptionName";
     private static final String EXCEPTION_MESSAGE_STUB = "message";
     private static final String EXCEPTION_DESCRIPTION_STUB = "description";
+    private static final String URL_PATH_VAR_NOT_LONG = "testPathVarNotLong";
+    private static final String URL_TEMPLATE = "/scrapper/tg-chat/{id}";
+    private static final long URL_PATH_VAR_LONG = 1L;
     @MockBean
     private ChatService chatService;
     @Autowired
@@ -36,30 +42,17 @@ class TelegramChatControllerTest {
     @DisplayName("Test POST \"chat sign up\" 200 OK")
     void testPostSuccess() throws Exception {
         mockMvc.perform(
-            post("/scrapper/tg-chat/{id}", 1L)
+            post(URL_TEMPLATE, URL_PATH_VAR_LONG)
         ).andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("Test POST \"chat sign up\" 400 BadRequest")
-    void testPostBadRequest() throws Exception {
-        mockMvc.perform(
-                post("/scrapper/tg-chat/{id}", "testPathVarNotLong")
-            )
-            .andExpect(status().isBadRequest())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath(ERROR_RESPONSE_DESC_PATH).value("Invalid or incorrect request parameters"))
-            .andExpect(jsonPath(ERROR_RESPONSE_CODE_PATH).value("400"))
-            .andExpect(jsonPath(ERROR_RESPONSE_EXCEPTION_NAME_PATH).value("MethodArgumentTypeMismatchException"));
     }
 
     @Test
     @DisplayName("Test POST \"chat sign up\" 409 Conflict")
     void testPostConflict() throws Exception {
         Mockito.doThrow(new ConflictException(EXCEPTION_MESSAGE_STUB, EXCEPTION_DESCRIPTION_STUB)).when(chatService)
-            .register(1L);
+            .register(URL_PATH_VAR_LONG);
         mockMvc.perform(
-                post("/scrapper/tg-chat/{id}", 1L)
+                post(URL_TEMPLATE, URL_PATH_VAR_LONG)
             )
             .andExpect(status().is(409))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -73,7 +66,7 @@ class TelegramChatControllerTest {
     @DisplayName("Test DELETE \"chat deletion\" 200 OK")
     void testDeleteSuccess() throws Exception {
         mockMvc.perform(
-            delete("/scrapper/tg-chat/{id}", 1L)
+            delete(URL_TEMPLATE, URL_PATH_VAR_LONG)
         ).andExpect(status().isOk());
     }
 
@@ -81,9 +74,9 @@ class TelegramChatControllerTest {
     @DisplayName("Test DELETE \"chat deletion\" 404 Not Found")
     void testDeleteNotFound() throws Exception {
         Mockito.doThrow(new NotFoundException(EXCEPTION_MESSAGE_STUB, EXCEPTION_DESCRIPTION_STUB)).when(chatService)
-            .unregister(1L);
+            .unregister(URL_PATH_VAR_LONG);
         mockMvc.perform(
-                delete("/scrapper/tg-chat/{id}", 1L)
+                delete(URL_TEMPLATE, URL_PATH_VAR_LONG)
             )
             .andExpect(status().is(404))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -91,6 +84,25 @@ class TelegramChatControllerTest {
             .andExpect(jsonPath(ERROR_RESPONSE_EXCEPTION_MESSAGE_PATH).value(EXCEPTION_MESSAGE_STUB))
             .andExpect(jsonPath(ERROR_RESPONSE_CODE_PATH).value("404"))
             .andExpect(jsonPath(ERROR_RESPONSE_EXCEPTION_NAME_PATH).value("NotFoundException"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideMocks")
+    @DisplayName("Test POST \"chat sign up\" and DELETE \"chat deletion\" 400 BadRequest")
+    void testPostBadRequest(MockHttpServletRequestBuilder builder) throws Exception {
+        mockMvc.perform(builder)
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath(ERROR_RESPONSE_DESC_PATH).value("Invalid or incorrect request parameters"))
+            .andExpect(jsonPath(ERROR_RESPONSE_CODE_PATH).value("400"))
+            .andExpect(jsonPath(ERROR_RESPONSE_EXCEPTION_NAME_PATH).value("MethodArgumentTypeMismatchException"));
+    }
+
+    private static MockHttpServletRequestBuilder[] provideMocks() {
+        return new MockHttpServletRequestBuilder[] {
+            post(URL_TEMPLATE, URL_PATH_VAR_NOT_LONG),
+            delete(URL_TEMPLATE, URL_PATH_VAR_NOT_LONG)
+        };
     }
 
 }
