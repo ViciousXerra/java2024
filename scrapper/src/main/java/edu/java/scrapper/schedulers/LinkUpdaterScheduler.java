6fdb22c8cd8cdcc6afe556a7753fdf8254f2ce15
Elijah <ilyasmolnikov567@gmail.com>
2024-03-16 12:06:37 +0300

    UpdaterScheduler almost done: created: Domain enum Parser

--- scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scrapper/schedulers/LinkUpdaterScheduler.java
@@ -8,7 +8,7 @@ import edu.java.scrapper.telegrambotclient.clients.BotUpdateClient;
 import edu.java.scrapper.webclients.clients.GitHubClient;
 import edu.java.scrapper.webclients.clients.StackOverFlowClient;
 import edu.java.scrapper.webclients.dto.github.RepositoryActivityResponse;
-import edu.java.scrapper.webclients.dto.stackoverflow.AnswerInfo;
+import edu.java.scrapper.webclients.dto.stackoverflow.QuestionInfo;
 import edu.java.scrapper.webclients.dto.stackoverflow.StackOverFlowQuestionResponse;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -16,13 +16,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import static edu.java.scrapper.schedulers.LinkUpdaterUtils.RESOURCE_PATTERN;
 
 @Component
 public class LinkUpdaterScheduler {
@@ -72,7 +72,7 @@ public class LinkUpdaterScheduler {
     }
 
     private void processGitHubUrl(Link link, Map<Link, ZonedDateTime> map) {
-        Matcher m = LinkUpdaterUtils.RESOURCE_PATTERN.matcher(link.url());
+        Matcher m = RESOURCE_PATTERN.matcher(link.url());
         if (m.find()) {
             List<RepositoryActivityResponse> activityList = gitHubClient.getActivityListResponse(
                 m.group(LinkUpdaterUtils.GITHUB_REPO_OWNER_GROUP),
@@ -88,14 +88,14 @@ public class LinkUpdaterScheduler {
     }
 
     private void processStackOverFlowUrl(Link link, Map<Link, ZonedDateTime> map) {
-        Matcher m = LinkUpdaterUtils.RESOURCE_PATTERN.matcher(link.url());
+        Matcher m = RESOURCE_PATTERN.matcher(link.url());
         if (m.find()) {
-            StackOverFlowQuestionResponse<AnswerInfo> answerInfo =
-                stackOverFlowClient.getAnswerInfoResponse(
+            StackOverFlowQuestionResponse<QuestionInfo> questionInfo =
+                stackOverFlowClient.getQuestionInfoResponse(
                     Long.parseLong(m.group(LinkUpdaterUtils.STACKOVERFLOW_QUESTION_ID_GROUP))
                 );
             ZonedDateTime lastActivityDateTime =
-                answerInfo.items().getFirst().lastActivityTime().atZoneSameInstant(ZoneOffset.UTC);
+                questionInfo.items().getFirst().lastActivityTime().atZoneSameInstant(ZoneOffset.UTC);
             if (lastActivityDateTime.isAfter(link.updatedAt())) {
                 map.put(link, lastActivityDateTime);
             }
@@ -105,7 +105,7 @@ public class LinkUpdaterScheduler {
     }
 
     @Transactional
-    private void modifyUpdatedAtAndReturnChatMap(Map<Link, ZonedDateTime> map) {
+    public void modifyUpdatedAtAndReturnChatMap(Map<Link, ZonedDateTime> map) {
         map.forEach((key, value) -> linkRepository.modifyUpdatedAtTimestamp(
             key.url(),
             value
@@ -113,36 +113,6 @@ public class LinkUpdaterScheduler {
     }
 
     private void startMessaging(Map<Link, ZonedDateTime> map) {
-
-    }
-
-    private static final class LinkUpdaterUtils {
-        private final static String RESOURCE_SLICER_REGEX =
-            "^(https|git)(://|@)([^/:]+)[/:]([^/:]+)/([^/:]+)(.+)$";
-        private final static Pattern RESOURCE_PATTERN = Pattern.compile(RESOURCE_SLICER_REGEX);
-        private final static int DOMAIN_NAME_GROUP = 3;
-        private final static int GITHUB_REPO_OWNER_GROUP = 4;
-        private final static int GITHUB_REPO_NAME_GROUP = 5;
-        private final static int STACKOVERFLOW_QUESTION_ID_GROUP = 5;
-
-        private static Domain resolveDomain(String url) {
-            Matcher matcher = RESOURCE_PATTERN.matcher(url);
-            if (matcher.find()) {
-                return switch (matcher.group(DOMAIN_NAME_GROUP)) {
-                    case "github.com" -> Domain.GITHUB;
-                    case "stackoverflow.com" -> Domain.STACKOVERFLOW;
-                    default -> throw new IllegalArgumentException("Unsupported domain: %s".formatted(matcher.group(
-                        DOMAIN_NAME_GROUP)));
-                };
-            } else {
-                throw new IllegalArgumentException("Unable to recognize URL pattern");
-            }
-        }
-
-        private enum Domain {
-            GITHUB,
-            STACKOVERFLOW
-        }
 
     }
 
